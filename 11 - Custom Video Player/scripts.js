dfb9bc2944dd6494262a5fa902d6a09733554ef2
Sebastian <sebastian.m.kaleta@gmail.com>
2019-04-04 22:49:46 +0200

    video with styles: add js functionality

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -3,9 +3,34 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress_filled');
+const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('.[data-skip]');
-const ranges = player.querySelector('.player_slider');
+const skipButtons = player.querySelector('[data-skip]');
+const ranges = player.querySelector('.player__slider');
 /* Build our functions */
+
+function togglePlay() {
+
+    const method = video.paused ? 'play' : 'pause';
+
+
+    // if(video.paused){
+    //     video.player();
+    // }else{video.pause();}
+
+    video[method]();
+
+}
+
+function updateButton() {
+    const icon = this.paused ? "play" : 'stop';
+
+    toggle.textContent = icon;
+}
+
 /* Hook up the event listeners*/
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
